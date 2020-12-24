8879a1f785d4c338b2092ecbc85078d543cb0978
Matt <peebles.mm@gmail.com>
2020-12-24 15:24:24 -0800

    begin style fix of main page

--- dist/app.js
+++ dist/app.js
@@ -69,19 +69,21 @@ function stickyNav()
 
 function smoothScroll()
 {
-	$('a[href*="#"]:not([href="#"])').on('click', function (e)
-	{
-		e.preventDefault();
+	// $('a[href*="#"]').on('click', function (e)
+	// {
+	// 	e.preventDefault();
 
-		if ($($.attr(this, 'href')).length > 0)
-		{
-			$('html, body').animate(
-				{
-					scrollTop: $($.attr(this, 'href')).offset().top
-				}, 400);
-		}
-		return false;
-	});
+	// 	console.log('yo')
+
+	// 	if ($($.attr(this, 'href')).length > 0)
+	// 	{
+	// 		$('html, body').animate(
+	// 			{
+	// 				scrollTop: $($.attr(this, 'href')).offset().top
+	// 			}, 400);
+	// 	}
+	// 	return false;
+	// });
 }
 
 function highlightCurrentSection()
